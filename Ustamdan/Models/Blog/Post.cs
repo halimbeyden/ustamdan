4c36709405880fd719300c5202c6ec8cfc4ff0bb
halimbeyden <8233692+halimbeyden@users.noreply.github.com>
2016-09-06 17:30:41 +0300

    Architecture

---
+++ Ustamdan/Models/Blog/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ustamdan.Models.Blog
+{
+    public enum PostType
+    {
+        Image,
+        Video
+    }
+    public enum Language
+    {
+        English,
+        Turkish
+    }
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string PostContent { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string AuthorId { get; set; }
+        public PostType Type { get; set; }
+        public bool IsPublished { get; set; }
+        public bool IsCommentEnabled { get; set; }
+        public bool IsSharingEnabled { get; set; }
+        public Language Language { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
---
+++ Ustamdan/Models/ErrorLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ustamdan.Models
+{
+    public class ErrorLog
+    {
+        public int Id { get; set; }
+        public DateTime ErrorDate { get; set; }
+        public string Message { get; set; }
+        public string Description { get; set; }
+    }
+}
