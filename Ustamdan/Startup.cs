9954f7787fcef43ab077db1cf5dd135d2d33330b
halimbeyden <8233692+halimbeyden@users.noreply.github.com>
2016-09-06 14:39:45 +0300

    fist commit

---
+++ Ustamdan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ustamdan.Startup))]
+namespace Ustamdan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
