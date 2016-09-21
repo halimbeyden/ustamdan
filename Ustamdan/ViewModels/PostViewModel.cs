02ca0799151f72b18d72bd24c9591de5b5e5c591
halimbeyden <8233692+halimbeyden@users.noreply.github.com>
2016-09-21 13:53:26 +0300

    admin general

--- Ustamdan/ViewModels/PostViewModel.cs
+++ Ustamdan/ViewModels/PostViewModel.cs
@@ -12,6 +12,7 @@ namespace Ustamdan.ViewModels
         public string Title { get; set; }
         public string MediaURL { get; set; }
         public PostType PostType { get; set; }
+        public PostStatus PostStatus { get; set; }
         public string Description { get; set; }
         public string Author { get; set; }
         public string Body { get; set; }
@@ -20,6 +21,7 @@ namespace Ustamdan.ViewModels
         public DateTime DateModified { get; set; }
         public int[] Categories { get; set; }
         public int[] Tags { get; set; }
+        public string[] TagNames { get; set; }
 
         public PostViewModel()
         {
@@ -33,11 +35,13 @@ namespace Ustamdan.ViewModels
             this.Body = post.PostContent;
             this.Language = post.Language;
             this.PostType = post.Type;
+            this.PostStatus = post.Status;
             this.MediaURL = post.MediaURL;
             try
             {
                 this.Categories = post.Categories.Select(x => x.Id).ToArray();
                 this.Tags = post.Tags.Select(x => x.Id).ToArray();
+                this.TagNames = post.Tags.Select(x => x.Name).ToArray();
                 this.DateCreated = post.DateCreated;
                 this.DateModified = post.DateModified;
                 this.Author = post.Author.UserName;
