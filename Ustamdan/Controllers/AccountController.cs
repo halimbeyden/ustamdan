45bc25af504559c2ae833b6729c8af1e1f909eae
halimbeyden <8233692+halimbeyden@users.noreply.github.com>
2016-10-21 16:09:32 +0300

    roles

--- Ustamdan/Controllers/AccountController.cs
+++ Ustamdan/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using Ustamdan.Models;
 
 namespace Ustamdan.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
