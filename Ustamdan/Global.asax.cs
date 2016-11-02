982a41f3b12ded1f5283e458c309f7b026bc734d
halimbeyden <halimbeyden@localhost>
2016-11-02 17:15:11 +0300

    post partial & search

--- Ustamdan/Global.asax.cs
+++ Ustamdan/Global.asax.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -19,6 +20,7 @@ namespace Ustamdan
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AntiForgeryConfig.SuppressXFrameOptionsHeader = true;
 
             using (var context = new ApplicationDbContext())
             {
