af919e72268cda8ee16116da74b1e9ee0a3e78ae
halimbeyden <halim.beyden@gmail.com>
2017-01-03 14:25:50 +0300

    translating routes & share buttons

--- Ustamdan/App_Start/RouteConfig.cs
+++ Ustamdan/App_Start/RouteConfig.cs
@@ -13,6 +13,36 @@ namespace Ustamdan
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.MapRoute(
+                "Blog", 
+                "{lang}/{weekly}", 
+                new { controller = "Home", action = "Blog" }, 
+                new { lang = "(tr)|(en)", weekly = "(yazihane)|(weekly)" }
+                );
+            routes.MapRoute(
+                "About",
+                "{lang}/{aboutus}",
+                new { controller = "Home", action = "About" },
+                new { lang = "(tr)|(en)", aboutus = "(hakkimizda)|(aboutus)" }
+                );
+            routes.MapRoute(
+               "Contact",
+               "{lang}/{contact}",
+               new { controller = "Home", action = "Contact" },
+               new { lang = "(tr)|(en)", contact = "(iletisim)|(contact)" }
+               );
+            routes.MapRoute(
+               "Search",
+               "{lang}/{search}",
+               new { controller = "Home", action = "Search" },
+               new { lang = "(tr)|(en)", search = "(ara)|(search)" }
+               );
+            routes.MapRoute(
+              "Post",
+              "{lang}/{weekly}/{id}/{title}",
+              new { controller = "Home", action = "Post" ,title = UrlParameter.Optional},
+              new { lang = "(tr)|(en)", weekly = "(yazihane)|(weekly)"}
+              );
+            routes.MapRoute(
                 name: "Language",
                 url: "{lang}/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
