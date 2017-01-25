e9d1c5d5c42637c8f116eb42c5a39a90c1e6c994
halimbeyden <8233692+halimbeyden@users.noreply.github.com>
2017-01-25 17:50:25 +0300

    IdeaCube started

---
+++ Ustamdan/Models/IdeaCube/Idea.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ustamdan.Models.IdeaCube
+{
+    public enum IdeaType
+    {
+        Bug,
+        Enhancement,
+        Proposal
+    }
+    public enum IdeaStatus
+    {
+        Created,
+        Approved,
+        Solved,
+        Deleted
+    }
+    public class Idea
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        public IdeaType Type { get; set; }
+        public IdeaStatus Status { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public string MediaUrl { get; set; }
+        public string CreatedById { get; set; }
+        public DateTime? ApprovedDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+        [ForeignKey("CreatedById")]
+        public virtual ApplicationUser CreatedBy { get; set; }
+        public virtual List<IdeaComment> IdeaComments { get; set; }
+        public virtual List<IdeaEmoticon> IdeaEmoticons { get; set; }
+        public List<IdeaComment> getComments()
+        {
+            return IdeaComments.Where(x => !x.Is_Deleted).ToList();
+        }
+    }
+}
