11e000daba016cb2ba379bcc2c16c1079821aaa2
halimbeyden <8233692+halimbeyden@users.noreply.github.com>
2017-01-26 17:22:14 +0300

    idea continue

--- Ustamdan/Controllers/IdeaCubeController.cs
+++ Ustamdan/Controllers/IdeaCubeController.cs
@@ -16,12 +16,14 @@ namespace Ustamdan.Controllers
         // GET: IdeaCube
         public ActionResult Index()
         {
-            using (var db  = new ApplicationDbContext())
+            using (var db = new ApplicationDbContext())
             {
                 var model = db.Ideas
-                    .Include(x=>x.CreatedBy)
+                    .Include(x => x.CreatedBy)
+                     .Include(x => x.IdeaComments)
+                    .Include(x => x.IdeaEmoticons)
                     .Where(x => x.Status != IdeaStatus.Deleted)
-                    .OrderBy(x=>x.CreationDate)
+                    .OrderByDescending(x => x.CreationDate)
                     .ToList();
                 return View(model);
             }
@@ -47,30 +49,31 @@ namespace Ustamdan.Controllers
             using (var db = new ApplicationDbContext())
             {
                 var model = db.Ideas
-                    .Include(x=>x.CreatedBy)
+                    .Include(x => x.CreatedBy)
                     .Include(x => x.IdeaComments)
                     .Include(x => x.IdeaEmoticons)
-                    .FirstOrDefault(x=>x.Id == id);
+                    .FirstOrDefault(x => x.Id == id);
                 if (model == null)
                     return HttpNotFound();
                 return View(model);
             }
         }
         [ValidateAntiForgeryToken()]
-        public ActionResult Comment(int idea,string content)
+        public ActionResult Comment(int idea, string content)
         {
             using (var db = new ApplicationDbContext())
             {
                 var model = db.Ideas.Find(idea);
                 if (model == null)
                     return HttpNotFound();
-                model.IdeaComments.Add(new IdeaComment() {
+                model.IdeaComments.Add(new IdeaComment()
+                {
                     Content = content,
                     CreationDate = DateTime.Now,
                     CreatedBy = User.Identity.GetUserId()
                 });
                 db.SaveChanges();
-                return RedirectToAction("Detail",new { id = idea});
+                return RedirectToAction("Detail", new { id = idea });
             }
         }
         [HttpPost]
@@ -87,7 +90,7 @@ namespace Ustamdan.Controllers
             }
         }
         [HttpPost]
-        public ActionResult DeleteComment(int idea,int comment)
+        public ActionResult DeleteComment(int idea, int comment)
         {
             using (var db = new ApplicationDbContext())
             {
@@ -97,6 +100,40 @@ namespace Ustamdan.Controllers
                 db.IdeaComments.Remove(model);
                 db.SaveChanges();
                 return RedirectToAction("Detail", new { id = idea });
+            }
+        }
+        [HttpPost]
+        public JsonResult PutEmotion(int idea, EmotionType emotion)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var model = db.Ideas.Find(idea);
+                if (model == null)
+                    return null;
+                bool addFlag = true;
+                var userEmotion = model.IdeaEmoticons.FirstOrDefault(x => x.CreatedBy == User.Identity.GetUserId());
+                if (userEmotion != null)
+                {
+                    var point = (int)userEmotion.Type;
+                    model.IdeaEmoticons.Remove(userEmotion);
+                    model.Score -= point;
+                    if (userEmotion.Type == emotion)
+                        addFlag = false;
+                }
+
+                if (addFlag)
+                {
+                    model.IdeaEmoticons.Add(new IdeaEmoticon()
+                    {
+                        CreatedBy = User.Identity.GetUserId(),
+                        Type = emotion,
+                        CreatedDate = DateTime.Now
+                    });
+                    model.Score += (int)emotion;
+                }
+                db.SaveChanges();
+                var result = model.IdeaEmoticons.GroupBy(x => x.Type).Select(x => new { Key = x.Key, Count = x.Count() });
+                return Json(result);
             }
         }
     }
