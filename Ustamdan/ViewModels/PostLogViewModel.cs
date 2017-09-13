d2f139f9c03d904d5225935b93cb1228e0e8bddd
halim.beyden <halim.beyden@gmail.com>
2017-09-13 12:41:43 +0000

    timeme

--- Ustamdan/ViewModels/PostLogViewModel.cs
+++ Ustamdan/ViewModels/PostLogViewModel.cs
@@ -9,25 +9,40 @@ namespace Ustamdan.ViewModels
 {
     public class PostLogViewModel
     {
-        public List<Tuple<PostViewModel,int>> MostViewed10Posts { get; set; }
+        public List<Tuple<PostViewModel,int,string>> MostViewed10Posts { get; set; }
         public List<Tuple<PostViewModel,DateTime, int>> MostViewedPostsAccordingToMonth { get; set; }
+
+        private List<PostLog> AllPostLogs { get; set; }
+        public List<Tuple<string,int>> UserAgents { get; set; }
+        public List<Tuple<string, int>> Authors { get; set; }
+
+        public List<Tuple<string,int,int>> MonthlyPostCounts { get; set; }
 
         public PostLogViewModel(ApplicationDbContext db)
         {
-            MostViewed10Posts = db.PostLogs
-                .Include("Posts")
+            AllPostLogs = db.PostLogs
+                .Where(x => !x.UserAgent.Contains("http") && !x.UserAgent.Contains("bot")).ToList();
+            MostViewed10Posts = AllPostLogs
                 .GroupBy(x => x.PostId)
                 .OrderByDescending(x => x.Count())
                 .Take(10)
-                .ToList()
-                .Select(x => Tuple.Create(new PostViewModel(x.First().Post), x.Count())).ToList();
+                .Select(x => Tuple.Create(new PostViewModel(x.First().Post), x.Count(),x.Sum(y=>y.ElapsedTime).ToString())).ToList();
 
-            MostViewedPostsAccordingToMonth = db.PostLogs
-                .Include("Posts")
+            MostViewedPostsAccordingToMonth = AllPostLogs
                 .GroupBy(x=> new {Year = x.Date.Year,Month = x.Date.Month,Post = x.PostId})
-                .ToList()
                 .Select(x=>Tuple.Create(new PostViewModel(x.First().Post),new DateTime(x.Key.Year,x.Key.Month,1),x.Count()))
                 .ToList();
+            UserAgents = AllPostLogs.GroupBy(x => x.UserAgent).Select(x=>Tuple.Create(x.Key,x.Count())).ToList();
+            Authors = AllPostLogs.GroupBy(x => x.Post.AuthorName).Select(x => Tuple.Create(x.Key,x.Count())).OrderBy(x=>x.Item2).ToList();
+            MonthlyPostCounts = AllPostLogs
+                .GroupBy(x=>new DateTime(x.Date.Year,x.Date.Month,1))
+                .OrderBy(x=>x.Key)
+                .Select(x=>
+                    Tuple.Create(x.Key.ToString("MMMM yyyy"),
+                                 x.Count(),
+                                 x.Select(y=>y.Ip).Distinct().Count()))
+                .ToList();
+             //db.Database.SqlQuery<Contact>("Contact_Search @LastName, @FirstName")
         }
         
     }
